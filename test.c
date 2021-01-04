f200bb62c3ba87d7ee0916e77b84f899f4b08aac
Anton Dutsov <anton@dutsov.com>
2021-01-04 16:48:18 +0200

    test

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void go_south_east(int lat, int lon)
+{
+    lat = lat - 1;
+    lon = lon +1;
+}
+
+int main(){
+    int latitude = 32;
+    int longitutde = -64;
+
+    go_south_east(latitude, longitutde);
+
+    printf("Avast! Now at: [%d, %d]\n", latitude, longitutde);
+    
+
+    return 0;
+}
